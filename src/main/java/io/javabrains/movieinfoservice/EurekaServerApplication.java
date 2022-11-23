45a598c1fe5ba21795d1ed7aacb81d2b42a804d8
Mayur Chokte <mayurchokte@gmail.com>
2022-11-23 16:55:26 +0530

    "Adding new statement."

--- src/main/java/io/javabrains/movieinfoservice/EurekaServerApplication.java
+++ src/main/java/io/javabrains/movieinfoservice/EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer 
 public class EurekaServerApplication {
-
+ 
 	public static void main(String[] args) {
 		System.out.println("Calling eureka server application.");
 		SpringApplication.run(EurekaServerApplication.class, args);
